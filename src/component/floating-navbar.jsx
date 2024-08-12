75b4c56a21eea2abac027adc8e738a01c9628888
Yash Brid <yashbrid03@gmail.com>
2024-08-12 16:23:45 +0530

    Added Socials

--- src/component/floating-navbar.jsx
+++ src/component/floating-navbar.jsx
@@ -9,6 +9,8 @@ import {
 import { cn } from "../lib/utils";
 import { Link } from "react-router-dom";
 import { Button } from "./moving-border";
+import { IconBrandGithub } from "@tabler/icons-react";
+import { IconBrandLinkedin } from "@tabler/icons-react";
 
 export const FloatingNav = ({ navItems, className }) => {
   const { scrollYProgress } = useScroll();
@@ -48,10 +50,11 @@ export const FloatingNav = ({ navItems, className }) => {
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit  fixed top-7 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-2 py-2  items-center justify-center space-x-4",
+          "flex max-w-fit  fixed top-7 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-5 pl-2 py-2  items-center justify-center space-x-4",
           className
         )}
       >
+        
         {navItems.map((navItem, idx) => (
           <Button
             borderRadius="1.75rem"
@@ -63,6 +66,12 @@ export const FloatingNav = ({ navItems, className }) => {
             </Link>
           </Button>
         ))}
+        <a href="https://github.com/yashbrid03">
+        <IconBrandGithub className="text-white  "/>
+        </a>
+        <a href="https://www.linkedin.com/in/yashbrid03/">
+        <IconBrandLinkedin className="text-white " />
+        </a>
       </motion.div>
     </AnimatePresence>
   );
